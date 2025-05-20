212620fb2d0c139adc55d04e50caa196a4c47a65
Ayush-Bansal08 <ab262133@gmail.com>
2025-05-21 00:01:03 +0530

    Update Projects.jsx

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -46,42 +46,6 @@ function Projects() {
             A Web3 Netflix-style platform using NFX token. Smart contracts (Solidity) handle payments and access. Integrated blockchain with Ethers.js and Web3.js.
           </h2>
         </div>
-=======
-function Projects(){
-    return(
-<> 
-<div className="bg-gray-600 w-70 ml-20 text-center mt-9 hover:scale-100 hover:shadow-2xl hover:shadow-purple-500">
-<a href="https://ayush-bansal08.github.io">
-<img src="https://st2.depositphotos.com/1032577/6582/i/950/depositphotos_65828845-stock-photo-portfolio-written-on-notebook.jpg" alt="" />
-</a>
-<h1 className="text-white font-bold">My first project was a basic personal profile page using HTML,CSS.</h1>
-</div>
-
-<div className="w-70 ml-127 mb-135 mt-[-240px] bg-gray-700 text-center hover:scale-100 hover:shadow-2xl hover:shadow-purple-500">
-<a href="https://ayush-bansal08.github.io/Netflix/">
-<img src="https://jobs.netflix.com/static/images/Netflix-Social-Rectangle.png" alt="" />
-</a>
-<h2 className="text-white font-bold">This is a project made using HYML,CSS,JAVASCRIPT. This project is not completed fully but i tried to make a similar login page just like Netflix. Here i made a cursor such that it changes colour every time it moves an inch.</h2>
-</div>
-
-<div className="w-70 ml-230 mb-135  mt-[-825px] bg-gray-700 text-center hover:scale-100 hover:shadow-2xl hover:shadow-purple-500">
-<a href="https://passwordgenerator-blond-chi.vercel.app/">
-<img src="https://img.freepik.com/free-photo/flat-lay-laptop-with-ethernet-cables-notebook-with-password_23-2148578080.jpg?ga=GA1.1.2029584824.1735299451&semt=ais_hybrid" alt="" />
-</a>
-<h2 className="text-white font-bold">This is a project made using HYML,CSS,JAVASCRIPT. Here oyu can easily generate your password which can include characters and numbers and the length of the password can also be decided by you.</h2>
-</div>
-
-<div className="w-70 ml-127 mb-135 mt-[-240px] bg-gray-700 text-center hover:scale-100 hover:shadow-2xl hover:shadow-purple-500">
-<a href="https://ayush-bansal08.github.io/Netflix/">
-<img src="https://jobs.netflix.com/static/images/Netflix-Social-Rectangle.png" alt="" />
-</a>
-<h2 className="text-white font-bold">This is a project made using HYML,CSS,JAVASCRIPT. This project is not completed fully but i tried to make a similar login page just like Netflix. Here i made a cursor such that it changes colour every time it moves an inch.</h2>
-</div>
-</>
-    )
->>>>>>> 3e646cb83004fd31233467a32eab00aae92c3c42
-
-        
       </div>
     </>
   );
